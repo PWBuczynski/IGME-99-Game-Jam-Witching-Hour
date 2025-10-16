0a5459f20a12c38a533cd376a8a8d345253ec2ff
PWBuczynski <pwb3561@rit.edu>
2025-10-16 15:13:13 -0400

    Added 10 locations for 5 coins to possibly spawn/move to

--- Witching Hour/Assets/Scripts/Player.cs
+++ Witching Hour/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,14 +8,52 @@ public class Player : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public static int score = 0;
+    public static int coinAmount = 5;
     float speed = 5.0f;
+    public GameObject coinPrefab;
+
+    public GameObject[] coinList = new GameObject[coinAmount];
+
+    public Vector3[] spawnList = new Vector3[10];
     
     public Text scoreAmount;
     
     //Nothing is needed in Start.
     void Start()
     {
+        //Possible locations for coin spawning
+        spawnList[0] = new Vector3(-6.54f,4.29f,0);
+        spawnList[1] = new Vector3(-4.36f, 4.29f, 0);
+        spawnList[2] = new Vector3(-8.69f, -1.04f, 0);
+        spawnList[3] = new Vector3(-2.51f, -1.39f, 0);
+        spawnList[4] = new Vector3(0, 2.50f, 0);
+        spawnList[5] = new Vector3(2.65f, -2.45f, 0);
+        spawnList[6] = new Vector3(3.83f, -0.67f, 0);
+        spawnList[7] = new Vector3(6.38f, 0.95f, 0);
+        spawnList[8] = new Vector3(6.32f, 2.38f, 0);
+        spawnList[9] = new Vector3(2.13f, 2.65f, 0);
 
+        for (int i = 0; i < coinAmount; i++)
+        {
+            Vector3 spawnPosition;
+            bool positionTaken;
+            do
+            {
+                positionTaken = false;
+                int randomInt = Random.Range(0, 10);
+                spawnPosition = spawnList[randomInt];
+                for (int j = 0; j < i; j++)
+                {
+                    if (coinList[j] != null && coinList[j].transform.position == spawnPosition)
+                    {
+                        positionTaken = true;
+                        break;
+                    }
+                }   
+            }
+            while (positionTaken);
+            coinList[i] = Instantiate(coinPrefab, spawnPosition, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
@@ -44,6 +83,7 @@ public class Player : MonoBehaviour
         if (collision.gameObject.tag == "Coins")
         {
             Destroy(collision.gameObject);
+            SpawnCoin(collision.gameObject);
             score++;
             scoreAmount.text = "Score: " + score;
         }
@@ -74,5 +114,11 @@ public class Player : MonoBehaviour
             }
         }
     }
-    
+    public GameObject SpawnCoin(GameObject coin)
+    {
+        int randomInt = Random.Range(0,10);
+        GameObject newObject = Instantiate(coinPrefab, spawnList[randomInt], Quaternion.identity);
+        Debug.Log($"{spawnList[randomInt].x} {spawnList[randomInt].y} {spawnList[randomInt].z}");
+        return newObject;
+    }
 }
